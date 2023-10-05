14b2088cf3984da58714cdd4308198e18cc95df2
Krzysztof Probulski <krzysztofprobulski@MacBook-Air-Krzysztof.local>
2023-10-05 11:02:49 +0200

    Area calculator

---
+++ Circle_Calculator/circle_calculator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    cout << "Radius (r): ";
+    double radius;
+    cin >> radius;
+    const double pi = 3.14;
+    double result = pi * pow(radius, 2);
+    cout << result;
+    return 0;
+    bool dupcia = true;
+    if dupcia
+
+
+}
